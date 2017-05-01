939fb96a8d24e8bc3ab759c7210d628d0c567ad0
DBeumans <buncho.gamingnetwork@gmail.com>
2017-05-01 14:48:56 +0200

    Fixed not ordered slot numbers in array.

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -1,48 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 using UnityEngine.UI;
 
-public class InventoryUI : MonoBehaviour {
+public class InventoryUI : MonoBehaviour
+{
 
     private Inventory itemDatabase;
 
-    [SerializeField]private GameObject inventoryPanel;
-    [SerializeField]private GameObject slotPanel;
-    [SerializeField]private GameObject inventorySlot;
-    [SerializeField]private GameObject inventoryItem;
+    [SerializeField]
+    private GameObject inventoryPanel;
+    [SerializeField]
+    private GameObject slotPanel;
+    [SerializeField]
+    private GameObject inventorySlot;
+    [SerializeField]
+    private GameObject inventoryItem;
 
-    private GameObject[] itemSlots;
 
     private List<Item> items = new List<Item>();
     private List<GameObject> slots = new List<GameObject>();
 
+    private List<GameObject> itemSlots = new List<GameObject>();
+
     private void Awake()
     {
         itemDatabase = GetComponent<Inventory>();
-        
-        itemSlots = GameObject.FindGameObjectsWithTag("ItemSlot");
-        Debug.Log(itemSlots.Length);
 
-        for (int i = 0; i < itemSlots.Length; i++)
+        // itemSlots = GameObject.FindGameObjectsWithTag("ItemSlot");
+
+
+        foreach (GameObject item in GameObject.FindGameObjectsWithTag("ItemSlot").OrderBy(name => name.name))
         {
-            slots.Add(itemSlots[i]);
-            items.Add(new Item());
+            itemSlots.Add(item);
         }
 
-        
+
+        Debug.Log(itemSlots.Count);
+
+        for (int i = 0; i < itemSlots.Count - 1; i++)
+        {
+            items.Add(new Item());
+            slots.Add(itemSlots[i]);
+
+        }
+
+
     }
 
     private void Start()
     {
-       // AddItem(0);
+        AddItem(0);
+        AddItem(1);
     }
 
     public void AddItem(int id)
     {
         Item itemToAdd = itemDatabase.FetchItemByID(id);
 
-        for (int i = 0; i < items.Count; i++)
+        for (int i = 0; i < items.Count - 1; i++)
         {
             if (items[i].ID == -1)
             {
@@ -51,7 +68,7 @@ public class InventoryUI : MonoBehaviour {
                 itemObject.transform.SetParent(slots[i].transform);
                 itemObject.transform.position = slots[i].transform.position;
                 itemObject.GetComponent<SpriteRenderer>().sprite = itemToAdd.Sprite;
-                print("added");
+
                 break;
             }
         }
