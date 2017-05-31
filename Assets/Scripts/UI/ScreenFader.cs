e744948e723b6c6b893736b43d276d86089da39f
DBeumans <buncho.gamingnetwork@gmail.com>
2017-05-31 13:59:31 +0200

    Added debug mode on the Screenfader script

--- Assets/Scripts/UI/ScreenFader.cs
+++ Assets/Scripts/UI/ScreenFader.cs
@@ -8,6 +8,16 @@ public class ScreenFader : MonoBehaviour {
     [SerializeField]private float _amountTofill = 1f;
     [SerializeField]private Image _image;
 
+    [SerializeField]private bool debugMode = false;
+
+    private void Start()
+    {
+        if(debugMode)
+        {
+            ScreenFadeOut();
+        }
+    }
+
     public void ScreenFadeOut()
     {
         _image.CrossFadeAlpha(0.0f, _amountTofill, true);
